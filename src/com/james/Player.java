527d1abb88d3ad02b4a0ebf1613d0c0e5b4ffde5
jamesyburr <vint.james@gmail.com>
2016-05-24 10:48:29 -0400

    Use string formatting

--- src/com/james/Player.java
+++ src/com/james/Player.java
@@ -16,7 +16,7 @@ public class Player {
     public void chooseName() {
         System.out.println("What is your name?");
         name = Main.nextLine();
-        System.out.println("Welcome, " + name);
+        System.out.printf("Welcome, %s\n", name);
     }
 
     public void chooseWeapon() throws Exception {
@@ -48,12 +48,12 @@ public class Player {
     }
 
     public void findItem(String item) {
-        System.out.println("You found " + item +"! Pick it up! [y/n]");
+        System.out.printf("You found %s! Pick it up! [y/n]\n", item);
         String answer = Main.nextLine();
         if (answer.equalsIgnoreCase("y")) {
             items.add(item);
 
-            System.out.println("You now have " + items.size() + " items");
+            System.out.printf("You now have %s items\n", items.size());
         }
     }
 }
